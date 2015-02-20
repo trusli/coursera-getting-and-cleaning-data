4ea779201f6b256241f472df39044eff3f09cb84
Teddy Rusli <teddy_rusli@yahoo.com>
2015-02-20 01:05:15 -0800

    Original file

---
+++ run_analysis.R
@@ -0,0 +1,25 @@
+############################################################################################
+#The purpose of this project is to demonstrate the ability to collect, work with, 
+#and clean a data set.
+
+#The goal is to prepare tidy data that can be used for later analysis.
+
+# Major steps:
+# 1. Merges the training and the test sets to create one data set.
+# 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# 3. Uses descriptive activity names to name the activities in the data set
+# 4. Appropriately labels the data set with descriptive variable names.   
+# 5. From the data set in step 4, creates a second, independent tidy data set with the 
+#    average of each variable for each activity and each subject.
+############################################################################################
+
+# download and prepare raw data data
+library(httr)
+fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+download.file(fileURL, destfile = "")
+list.files(".")
+dateDownLoaded <- date()
+
+
+# read raw data and load to data.frame
+
